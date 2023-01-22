11660bf560b2da6e228ef0e8c56e6f47a6c0b64c
Your Name <you@example.com>
2023-01-22 14:07:23 +0400

    third commit

--- index.js
+++ index.js
@@ -20,12 +20,21 @@ const notifactionDetails = {
         text: 'Notification text',
         svg:'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#3498db" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>'
     },
-    cancel:'<svg class="cancel" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"> <line x1="18" y1="6" x2="6" y2="18"></line> <line x1="6" y1="6" x2="18" y2="18"></line> </svg>'
+    cancel:'<svg onclick=deleteNotification(this.parentElement) class="cancel" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"> <line x1="18" y1="6" x2="6" y2="18"></line> <line x1="6" y1="6" x2="18" y2="18"></line> </svg>'
     
 }
 
+const deleteNotification = (toast) =>{
+
+    toast.classList.add('hide')
+    setInterval(() => {
+        toast.remove()
+    }, 400);
+
+}
+
   
-const 
+
 
 
 const createNotification = (id) => {
@@ -44,6 +53,9 @@ const createNotification = (id) => {
         ${cancelBtn}
     `;
   notifications.appendChild(notification);
+  setTimeout(() => {
+    deleteNotification(notification)
+  }, 5000);
 };
 
 buttons.childNodes.forEach((button) => {
